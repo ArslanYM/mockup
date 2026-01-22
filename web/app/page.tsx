6a863f3993c003ddbb9b95337f871d19cfdb2186
ArslanYM <arsalanyaqmalik@gmail.com>
2026-01-22 23:17:36 +0530

    feat: landing feats

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { Pricing } from "@/components/pages/pricing";
 import { Header } from "./_shared/Header";
 import Hero from "./_shared/Hero";
+import { FeaturesSection } from "@/components/pages/features";
 
 export default function Page() {
   return (
     <>
       <Header />
       <Hero />
+      <FeaturesSection />
+      <Pricing />
     </>
   );
 }
